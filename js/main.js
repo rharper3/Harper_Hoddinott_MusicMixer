83b76dd812d74532daed6199f95f01c6643bb408
Harper <r_harper3@tss-hr420-dm04.local>
2019-04-01 14:49:36 -0400

    updated some JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 (() => {
 	console.log("ready");
 
+	let draggableImages = document.querySelectorAll('#mainContent img');
+
+	// set up the drag events and then dragover and drop
 	
+	function initDrag() {
+		draggableImages.forEach(img => {
+			img.addEventListener("dragstart", function(e)) {
+				console.log("draggin...")
+				e.dataTransfer.setData("text/plain", this.id)
+			}
+		});
+	};
 	
 })();
